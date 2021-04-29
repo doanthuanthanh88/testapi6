abebdb593cd1ee6f86b7548bfab7a8709c90ac6b
Đoàn Thuận Thành <doanthuanthanh88@gmail.com>
2021-04-29 22:34:18 +0700

    Update libs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-const Module = require('module');
-const ROOT = __dirname + '/'
-
-const _require = Module.prototype.require
-Module.prototype.require = function reallyNeedRequire(name) {
-  if (name?.startsWith('testapi6/')) {
-    return _require.call(this, name.replace('testapi6/dist/', ROOT))
-  }
-  return _require.call(this, name)
-}
+import './init'
 
 import { InputYamlFile, main } from '@/main'
 
