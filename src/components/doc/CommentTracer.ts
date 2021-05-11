dbb8bbeb815140180f66dc2936bd1bfb3cacaceb
Đoàn Thuận Thành <doanthuanthanh88@gmail.com>
2021-05-12 01:15:14 +0700

    Update comment flow

--- src/components/doc/CommentTracer.ts
+++ src/components/doc/CommentTracer.ts
@@ -8,9 +8,22 @@ import chalk from 'chalk'
 
 class Group {
   static all = {}
-  name1?: string
+  private static _tab = []
+
+  push() {
+    Group._tab.push('  ')
+  }
+
+  pop() {
+    Group._tab.pop()
+  }
+
+  get tab() {
+    return Group._tab.join('')
+  }
 
   steps: {
+    tab?: string
     key?: string
     des?: string
     ref?: string
@@ -87,27 +100,103 @@ export class CommentTracer extends Tag {
     }
   }
 
+  private handleMMDSequence(line: string, group: Group) {
+    let m = line.match(/\[\+ (if|loop|parallel|alt|par)\](.*)/i)     // [+ if] Name is string
+    if (m) {
+      // ref
+      m[1] = m[1]?.trim().toUpperCase()
+      m[1] = m[1] === 'ALT' ? 'IF' : m[1] === 'PARALLEL' ? 'PAR' : m[1]
+      group.steps.push({
+        tab: group.tab,
+        des: (m[1] == 'IF' ? 'ALT ' : `${m[1]} `) + m[2],
+        tag: 'if'
+      })
+      group.push()
+    } else {
+      m = line.match(/\[\+ (else|end|and)\](.*)/i)     // [+ else] Name is number
+      if (m) {
+        group.pop()
+        m[1] = m[1]?.trim()
+        m[2] = m[2]?.trim()
+        group.steps.push({
+          tab: group.tab,
+          des: m[1].toUpperCase() + ' ' + m[2],
+          tag: m[1]
+        })
+        if (m[1].toUpperCase() !== 'END') group.push()
+      } else {
+        m = line.match(/\[\+ (note_right|note_left|note_over):([^\]]+)\](.*)/i)     // [+ else] Name is number
+        if (m) {
+          m[1] = m[1]?.trim()
+          m[2] = m[2]?.trim()
+          m[3] = m[3]?.trim()
+          group.steps.push({
+            tab: group.tab,
+            des: 'Note ' + (m[1] === 'NOTE_LEFT' ? 'left of ' : m[1] === 'NOTE_RIGHT' ? 'right of ' : 'over ') + m[2] + ': ' + m[3],
+            tag: m[1]
+          })
+        } else {
+          m = line.match(/\[\+ ([^\s]+)\s?([^\s]+)?([^\]]*)?\](.*)/i)     // [+ Service -> UserService] Get something here
+          if (m) {
+            m[1] = m[1]?.trim()
+            m[2] = m[2]?.trim()
+            m[3] = m[3]?.trim()
+            m[4] = m[4]?.trim()
+            let dir = '->>'
+            if (m[2] === '->') {
+              dir = '-->>'
+            } else if (m[2] === '<-') {
+              dir = '<<--'
+            } else if (m[2] === '>') {
+              dir = '->>'
+            } else if (m[2] === '<') {
+              dir = '<<--'
+            } else if (m[2] === '=>') {
+              dir = '-)'
+            } else if (m[2] === '<=') {
+              dir = '(--'
+            }
+            if (m[3]) {
+              group.steps.push({
+                tab: group.tab,
+                des: `${m[1]} ${dir} ${m[3]}: ${m[4]}`,
+                tag: ''
+              })
+            } else {
+              group.steps.push({
+                tab: group.tab,
+                des: `${m[1]} ${dir} ${m[1]}: ${m[4]}`,
+                tag: ''
+              })
+            }
+          }
+        }
+      }
+    }
+    return !!m
+  }
+
   private _scan(file: string) {
     return new Promise((resolve, reject) => {
       try {
         const rl = createInterface({ input: createReadStream(file) });
         let group: Group
         let ns = ''
-        rl.on('line', function (line) {
-          let m = line.match(/\[>(.*?)](.*)/i) // [> functionName] Test function
+        rl.on('line', (line) => {
+          let m = line.match(/\[>(.*?)]/i) // [> functionName]
           if (m) {
             // group
             m[1] = m[1]?.trim()
-            m[2] = m[2]?.trim()
             if (!group) {
-              if (m[1][0] === '>') {            // [> ^functionName] Begin scan this function
+              if (m[1][0] === '>') {            // [>> functionName]
                 m[1] = m[1].substr(1).trim()
-                group = new Group(ns, m[1], m[2], true)
+                group = new Group(ns, m[1], '', true)
               } else {
-                group = new Group(ns, m[1], m[2])
+                group = new Group(ns, m[1], '')
               }
+              group.push()
             } else {                            // [>] End function
-              group.name1 = m[2]
+              group.pop()
               if (CommentTracer.all[group.key]) throw new Error('Duplicate group ' + group.name)
               CommentTracer.all[group.key] = group
               group = undefined
@@ -119,21 +208,29 @@ export class CommentTracer extends Tag {
               m[1] = m[1]?.trim()
               m[2] = m[2]?.trim()
               group.steps.push({
+                tab: group.tab,
                 des: m[2],
                 ref: m[1],
                 tag: '<'
               })
+              group.push()
             } else {
               m = line.match(/\[-\](.*)/i)
               if (m) {
                 group.steps.push({
+                  tab: group.tab,
                   des: m[1].trim(),
                   tag: '-'
                 })
               } else {
-                m = line.match(/\[(.*?)\]/i)
-                if (m) {
-                  ns = m[1].trim()
+                if (this.type === 'mmd:sequence') {
+                  const isHandle = this.handleMMDSequence(line, group)
+                  if (!isHandle && !ns) {
+                    m = line.match(/\[(\w+)\]/i)
+                    if (m) {
+                      ns = m[1].trim()
+                    }
+                  }
                 }
               }
             }
@@ -166,27 +263,27 @@ export class CommentTracer extends Tag {
 
   print() {
     const writer = this.saveTo ? createWriteStream(this.saveTo) : null
+    writer?.write('sequenceDiagram\r\n')
     const outGroup = (h: Group, des: string, tab = '') => {
-      const msg = this.format(des || h.name)
-      context.group('↳ ' + msg)
-      writer?.write(tab + msg + '\r\n')
+      let msg = this.format(des || h.name)
+      if (msg) {
+        context.group(h.tab + '↳ ' + msg)
+        writer?.write(tab + h.tab + msg + '\r\n')
+      } else {
+        context.group('')
+        writer?.write('\r\n')
+      }
       tab += '  '
       h.steps?.forEach(step => {
         if (step.group) {
           outGroup(step.group, step.des, tab)
         } else {
           const msg = this.format(step.des)
-          context.log('↳ ' + msg)
-          writer?.write(tab + msg + '\r\n')
+          context.log(step.tab + '↳ ' + msg)
+          writer?.write(tab + step.tab + msg + '\r\n')
         }
       })
       context.groupEnd()
-      tab = tab.replace('  ', '')
-      if (!des && h.name1) {
-        const msg = this.format(h.name1)
-        context.log('↳ ' + msg)
-        writer?.write(tab + msg + '\r\n')
-      }
     }
     this.group.forEach(g => outGroup(g, undefined))
     writer?.close()
