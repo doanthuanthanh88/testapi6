e1788c4ec3d9076e6b76c6685de600cda986dbd3
Đoàn Thuận Thành <doanthuanthanh88@gmail.com>
2021-07-20 22:11:53 +0700

    Update comment

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import './init'
 import { InputYamlFile, main } from '@/main'
+import { program } from 'commander'
+import { existsSync } from 'fs'
+import { join } from 'path'
 
-// main(new InputYamlFile(process.argv[2]), process.argv[3], process.argv[4])
+// setTimeout(async () => {
+//   main(new InputYamlFile(process.argv[2]), process.argv[3], process.argv[4])
+// }, 500)
 
-setTimeout(async () => {
-  main(new InputYamlFile(process.argv[2]), process.argv[3], process.argv[4])
-}, 500)
+const packageJson = [join(__dirname, '../package.json'), join(__dirname, './package.json')].find(src => existsSync(src))
+const { version, description, name, repository } = require(packageJson)
+
+const cmd = program
+  .name(name)
+  .version(version, '', description)
+  .argument('<file>', 'Scenario path or file', undefined, 'index.yaml')
+  .argument('[password]', 'Password to decrypt scenario file')
+  .option('-e, --env <json_env>', 'Environment variable')
+  .addHelpText('after', `More:\n  ${repository.url}`)
+  .parse(process.argv)
+
+const [yamlFile, password] = cmd.args
+const { env = '{}' } = cmd.opts()
+
+main(new InputYamlFile(yamlFile), password, eval(env))
